3677f875e70b9bc22a6d8fdfa35d9253bd83e389
shivam.porwal <shivam@trustwork.com>
2020-06-06 22:20:51 +0530

    added weather api call in service and component. Now going for dinner break for 30 mins.

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -1,14 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {WeatherService} from '../../services/weather.service';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html'
 })
 export class WeatherComponent implements OnInit {
+  @Input() capitalWithCode: string;
+  weatherInformation: any;
 
-  constructor() { }
+  constructor(private weatherService: WeatherService) {
+  }
 
   ngOnInit(): void {
+    console.log('WeatherComponent loaded.');
   }
+
+  getWeatherInformation() {
+    if (!this.capitalWithCode) {
+      return;
+    }
+    this.weatherService.getWeatherInformation(this.capitalWithCode).subscribe(resp => {
+      this.weatherInformation = resp;
+    }, error => {
+      console.log(error);
+    });
+  }
+
 
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
+  private readonly baseUrl = 'http://api.openweathermap.org/data/2.5/weather';
+  private readonly appId = '794ee95e63c5a32aaf88cd813fa2e425';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getWeatherInformation(capitalWithCode: string) {
+    return this.http.get(this.baseUrl, {
+      params: {
+        q: capitalWithCode,
+        APPID: this.appId
+      }
+    });
+  }
 }
