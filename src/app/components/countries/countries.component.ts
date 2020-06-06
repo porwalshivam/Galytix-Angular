8cf9a08c151b8d0bdb504b4c0c54c1478ac0636d
shivam.porwal <shivam@trustwork.com>
2020-06-06 22:04:13 +0530

    added search functionality with debounce time of 500ms and added highlight search term in country name.

--- src/app/components/countries/countries.component.ts
+++ src/app/components/countries/countries.component.ts
@@ -1,16 +1,20 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {CountriesService} from '../../services/countries.service';
+import {Subject, Subscription} from 'rxjs';
+import {debounceTime, distinctUntilChanged} from 'rxjs/operators';
 
 @Component({
   selector: 'app-countries',
   templateUrl: './countries.component.html'
 })
-export class CountriesComponent implements OnInit {
+export class CountriesComponent implements OnInit, OnDestroy {
   countries = [];
   page = 1;
   pageSize = 10;
   loading = false;
   searchTerm = '';
+  searchTermChanged: Subject<string> = new Subject<string>();
+  searchTermSubscription: Subscription;
 
   constructor(private countriesService: CountriesService) {
   }
@@ -18,14 +22,25 @@ export class CountriesComponent implements OnInit {
   ngOnInit(): void {
     console.log('countries component loaded.');
     this.getCountries();
+    this.searchTermSubscription = this.searchTermChanged
+      .pipe(debounceTime(500), distinctUntilChanged())
+      .subscribe(searchTerm => this.getCountries(searchTerm));
   }
 
-  private getCountries() {
-    this.countriesService.getCountries().subscribe((resp: Array<any>) => {
+  private getCountries(searchText: string = '') {
+    this.countriesService.getCountries(searchText).subscribe((resp: Array<any>) => {
       this.countries = resp;
     }, error => {
       console.error(error);
     });
+  }
+
+  searchCountries(searchText: string) {
+    this.searchTermChanged.next(searchText);
+  }
+
+  ngOnDestroy(): void {
+    this.searchTermSubscription.unsubscribe();
   }
 
 }
